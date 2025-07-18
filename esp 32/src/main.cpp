61276ca31801fbb048acfe2e4752b64a8cbcd812
Pittermanifique <isaac.lestage@gmail.com>
2025-07-18 21:52:18 +0200

    adaptation du programe esp 32

--- esp 32/src/main.cpp
+++ esp 32/src/main.cpp
@@ -1,14 +1,15 @@
 #include <M5Unified.h>
+#include <Arduino.h>
+
+
+const int outputPin = 26;
 
 void setup() {
   // Initialize M5Stack
   M5.begin();
-
-  // Initialize the display
-  M5.Lcd.begin();
-  // Set up other components or configurations as needed
   M5.Lcd.setTextSize(2);
-  M5.Lcd.println("M5Stack Initialized");
+  M5.Lcd.println("M5Stack StickC Plus2 Example");
+  M5.Lcd.setCursor(0, 20);
 
   Serial.begin(115200);
 }
@@ -18,8 +19,11 @@ void loop() {
     String ligne = Serial.readStringUntil('\n');
     if (ligne == "1") {
       M5.Lcd.clear();
-      M5.Lcd.drawCircle(120, 120, 50, TFT_GREEN);
+      M5.Lcd.drawString("LED ON", 0, 20);
+      digitalWrite(outputPin, LOW);
+      digitalWrite(outputPin, HIGH);
     } else if(ligne == "0") {
+      digitalWrite(outputPin, LOW);
       M5.Lcd.clear();
     }
   }
